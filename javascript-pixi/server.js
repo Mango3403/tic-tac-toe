f3859655819d19b739e82f352eee15ab5bd5b5fc
Mango3403 <wzdsrhm1994@gmail.com>
2021-12-16 22:29:11 +0800

    update client server

---
+++ javascript-pixi/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+    console.log('Your files will be served through this web server at http://127.0.0.1:' + app.get('port'));
+});
+
+console.log('the server is started.');
